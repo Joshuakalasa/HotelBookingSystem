2aede024df3f0cfa29109f85f18bec5e099b467a
mwala <mwalamulenga.zt@gmail.com>
2023-02-03 10:23:27 +0200

    hotfix

--- README.md
+++ README.md
@@ -139,8 +139,6 @@ Don't forget to give the project a star! Thanks again!
 
 Distributed under the MIT License. See `LICENSE.txt` for more information.
 
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
-
 
 <!-- ACKNOWLEDGMENTS -->
 ## Acknowledgments
--- booking/models.py
+++ booking/models.py
@@ -73,7 +73,7 @@ class Reservation(models.Model):
     details = models.TextField(max_length=255, blank=True, null=True)
 
     def __str__(self):
-        return "{} {}".format(self.room, self.details)
+        return "{}".format(self.room)
 
 class Service(models.Model):
     name = models.CharField(max_length=255, blank=False, null=True)
